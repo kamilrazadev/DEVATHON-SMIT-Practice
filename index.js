a864694a415a720e24481724a980de41bc254221
Kamil Raza <m.kamilrazabhatti@gmail.com>
2023-10-27 17:16:30 +0500

    changes

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const exxpress = require("express");
 const app = exxpress();
 require("dotenv").config();
+const cors = require("cors");
 
 const port = process.env.SERVER_PORT;
 
+app.use(cors());
 app.use(exxpress.json());
 app.use("/api", require("./api/user/Router"));
 
